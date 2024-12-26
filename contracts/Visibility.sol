a484881ba609317e0278ef8a3fa27519d545a8ee
Cythonic1 <jfjbn80@gmail.com>
2024-12-26 18:49:21 +0800

    Visibility and functions

--- contracts/Visibility.sol
+++ contracts/Visibility.sol
@@ -3,3 +3,31 @@
 
 pragma solidity ^0.8.28;
 
+
+contract Visibility{
+    // Public allow anyone to see the function and use it even if 
+    // Also other contract can use it
+    // The wallat you deploy the smart contract on.
+    string public  hi = "Hi mohe";
+    function getString() public view returns (string memory){
+        return hi;
+    }
+
+    // Private allow you to use the function or the var inside the contract only
+    function changeString() private  returns (string memory){
+        hi = "Hello mom";
+        return hi ;
+    }
+
+    //internal this can be used in the contract and the inherent contracrs
+    function returnString() internal returns (string memory){
+        hi = "Hi mohe";
+        return hi ;
+    }
+
+    // external like saying we allow for external contracts to access it.
+    function helloOutSide() external returns (string memory){
+        hi = "Hi everyone";
+        return hi ;
+    }
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,8 @@ require("@nomicfoundation/hardhat-toolbox");
 const { ethers } = require("hardhat");
 
 async function deploy() {
-    const Greeting = await ethers.getContractFactory("Greeting");
+    // When deploy we focus on the name of the contract it self not the file name of the contract;
+    const Greeting = await ethers.getContractFactory("Visibility");
     const greeting = await Greeting.deploy();
     await greeting.waitForDeployment();
 
@@ -11,9 +12,12 @@ async function deploy() {
 }
 
 async function main() {
-    const greeting = await deploy();  // Call deploy and get the greeting contract
-    console.log(await greeting.getGreeting());  // Assuming `getGreeting()` is a function in the contract
-    console.log(await greeting.add(1, 200));
+    const vis = await deploy();  // Call deploy and get the greeting contract
+    // console.log(await greeting.getGreeting());  // Assuming `getGreeting()` is a function in the contract
+    // console.log(await greeting.add(1, 200));
+    console.log(await vis.getString());
+    console.log(await vis.helloOutSide());
+
 }
 
 main();
